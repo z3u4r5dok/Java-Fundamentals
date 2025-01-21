0986f996e18ae58ef35e41beaffa360573917267
z3u4r5dok <z3u4r5dok@gmail.com>
2025-01-21 14:37:53 -0500

    Example9 created for testing purposes

---
+++ src/labs_examples/fundamentals/examples/Example9.java
@@ -0,0 +1,11 @@
+package labs_examples.fundamentals.examples;
+
+import java.net.SocketOption;
+
+public class Example9 {
+    public static void main(String[] args) {
+        System.out.println("example 09, test works!!!");
+
+
+    }
+}
